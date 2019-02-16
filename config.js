aeefa4e26750d6e1c831a4124e640c1880d56da4
asdf <rednuke123@gmail.com>
2019-02-16 01:47:02 -0600

    asdf
    asdf

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: process.env.access_token;
-  consumer_secret: process.env.access_token_secret;
-  access_token: process.env.consumer_key;
-  access_token_secret: process.env.consumer_secret;
+  consumer_key: process.env.access_token,
+  consumer_secret: process.env.access_token_secret,
+  access_token: process.env.consumer_key,
+  access_token_secret: process.env.consumer_secret
 }
