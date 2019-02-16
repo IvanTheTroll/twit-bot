292093215fe61f9a180d5f55a4f0eaaa4b89a591
asdf <rednuke123@gmail.com>
2019-02-16 00:23:52 -0600

    asdf
    asdf

--- config.js
+++ config.js
@@ -6,8 +6,5 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: 'KFkQGpokotNoE8lOtpU65CMGv',  
-  consumer_secret: 'CaVy3JxfTvvs9CzlKIgDShDY0fB4dCfAFeHlqIFJcq4Gr693w2',
-  access_token: '1093608716472061953-iHu2oukkli2GTWUSgYdUfvzWtLU6m7',  
-  access_token_secret: 'MEY7MzzAifZLKZajo0hr1NA6azmPstDdePYMLwM5DmnSz'
+  
 }
