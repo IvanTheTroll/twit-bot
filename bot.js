c6a2ab58134f9c667c68bb30128cf1fda2407ceb
asdf <rednuke123@gmail.com>
2019-02-19 01:28:57 -0600

    Revert "Revert "asdf""
    This reverts commit a549685117ef496fd374ce51050f227edea5755a.

--- bot.js
+++ bot.js
@@ -13,12 +13,13 @@ var T = new twit({
 //});
 
 
-var stream = T.stream('statuses/filter', { track: ['@FreeSpeech4You fosscad'] });
-var stream2 = T.stream('statuses/filter', { track: ['@FreeSpeech4You magazine'] });
+var stream = T.stream('statuses/filter', { track: ['@FreeSpeech4You'] });
+//var stream2 = T.stream('statuses/filter', { track: ['@FreeSpeech4You magazine'] });
 stream.on('tweet', tweetEvent);
-stream2.on('tweet', tweetEvent2);
+//stream2.on('tweet', tweetEvent2);
 
 function tweetEvent(tweet) {
+
 
     // Who sent the tweet?
     var name = tweet.user.screen_name;
@@ -26,7 +27,9 @@ function tweetEvent(tweet) {
 	var quotedText = tweet.text;
 	var split = quotedText.split(" ");
 	var handles = [];
-	
+	if(quotedText.includes("fosscad"||"Fosscad")){
+		
+
 	//console.log(quotedText);
 	for(var i = 0; i < split.length-1; i++){
 		//console.log('split[i]: ' + split[i]);
@@ -68,9 +71,68 @@ function tweetEvent(tweet) {
         console.log('Tweeted: ' + params.status);
       }
     })
+	}else
+	if(quotedText.includes("magazine"||"Magazine")){
+		
+
+	//console.log(quotedText);
+	for(var i = 0; i < split.length-1; i++){
+		//console.log('split[i]: ' + split[i]);
+		if(split[i].startsWith("@")){
+			if(split[i] == '@freespeech4you'){
+			
+			}
+			else{
+				if(split[i] == '@FreeSpeech4You'){
+					
+				}else{
+				handles.push(split[i]);
+				}
+			}
+			//console.log('handles.tostring: ' + handles.toString());
+		}
+	}
+	
+    // What is the text?
+    // var txt = tweet.text;
+    // the status update or tweet ID in which we will reply
+    
+
+     // Get rid of the @ mention
+    // var txt = txt.replace(/@FreeSpeech4You/g, "");
+
+    // Start a reply back to the sender
+	var stringify = handles.toString();
+    var reply = "You want files to print a STANDARD CAPCAITY Magazine @" + name + ' ?' + '\nHere\s the files: https://github.com/maduce/fosscad-repo/tree/master/Rifles/AR-15_30_Round_Magazine-Ivanthetroll\n, along with proof of the mag in action: https://www.bitchute.com/video/VBzL8nKCCDzM/' + stringify;
+    var params             = {
+                              status: reply,
+                              in_reply_to_status_id: nameID
+                             };
+
+    T.post('statuses/update', params, function(err, data, response) {
+      if (err !== undefined) {
+        console.log(err);
+      } else {
+        console.log('Tweeted: ' + params.status);
+      }
+    })
+	}else
+	var reply = "I don't recognize that command.";
+    var params             = {
+                              status: reply,
+                              in_reply_to_status_id: nameID
+                             };
+
+    T.post('statuses/update', params, function(err, data, response) {
+      if (err !== undefined) {
+        console.log(err);
+      } else {
+        console.log('Tweeted: ' + params.status);
+      }
+    })
 };
 
-function tweetEvent2(tweet) {
+/*function tweetEvent2(tweet) {
 
     // Who sent the tweet?
     var name = tweet.user.screen_name;
@@ -121,3 +183,4 @@ function tweetEvent2(tweet) {
       }
     })
 };
+*/
