7b22518eacbbd182fd1aa27e7196b70a06240bbd
asdf <rednuke123@gmail.com>
2019-02-19 01:11:49 -0600

    fff
    ffff

--- bot.js
+++ bot.js
@@ -27,6 +27,56 @@ function tweetEvent(tweet) {
 	var quotedText = tweet.text;
 	var split = quotedText.split(" ");
 	var handles = [];
+	
+	
+	if(quotedText.includes("beep")){
+		
+
+	//console.log(quotedText);
+	for(var i = 0; i < split.length-1; i++){
+		//console.log('split[i]: ' + split[i]);
+		if(split[i].startsWith("@")){
+			if(split[i] == '@freespeech4you'){
+			
+			}
+			else{
+				if(split[i] == '@FreeSpeech4You'){
+					
+				}else{
+				handles.push(split[i]);
+				}
+			}
+			//console.log('handles.tostring: ' + handles.toString());
+		}
+	}
+	
+    // What is the text?
+    // var txt = tweet.text;
+    // the status update or tweet ID in which we will reply
+    
+
+     // Get rid of the @ mention
+    // var txt = txt.replace(/@FreeSpeech4You/g, "");
+
+    // Start a reply back to the sender
+	var stringify = handles.toString();
+    var reply = "@" + name + ' boop';
+    var params             = {
+                              status: reply,
+                              in_reply_to_status_id: nameID
+                             };
+
+    T.post('statuses/update', params, function(err, data, response) {
+      if (err !== undefined) {
+        console.log(err);
+      } else {
+        console.log('Tweeted: ' + params.status);
+      }
+    })
+	}
+	
+	
+	
 	if(quotedText.includes("fosscad"||"Fosscad")){
 		
 
@@ -163,56 +213,3 @@ function tweetEvent(tweet) {
     })
 	}
 };
-
-/*function tweetEvent2(tweet) {
-
-    // Who sent the tweet?
-    var name = tweet.user.screen_name;
-	var nameID  = tweet.id_str;
-	var quotedText = tweet.text;
-	var split = quotedText.split(" ");
-	var handles = [];
-	
-	//console.log(quotedText);
-	for(var i = 0; i < split.length-1; i++){
-		//console.log('split[i]: ' + split[i]);
-		if(split[i].startsWith("@")){
-			if(split[i] == '@freespeech4you'){
-			
-			}
-			else{
-				if(split[i] == '@FreeSpeech4You'){
-					
-				}else{
-				handles.push(split[i]);
-				}
-			}
-			//console.log('handles.tostring: ' + handles.toString());
-		}
-	}
-	
-    // What is the text?
-    // var txt = tweet.text;
-    // the status update or tweet ID in which we will reply
-    
-
-     // Get rid of the @ mention
-    // var txt = txt.replace(/@FreeSpeech4You/g, "");
-
-    // Start a reply back to the sender
-	var stringify = handles.toString();
-    var reply = "You want files to print a STANDARD CAPCAITY Magazine @" + name + ' ?' + '\nHere\s the files: https://github.com/maduce/fosscad-repo/tree/master/Rifles/AR-15_30_Round_Magazine-Ivanthetroll\n, along with proof of the mag in action: https://www.bitchute.com/video/VBzL8nKCCDzM/' + stringify;
-    var params             = {
-                              status: reply,
-                              in_reply_to_status_id: nameID
-                             };
-
-    T.post('statuses/update', params, function(err, data, response) {
-      if (err !== undefined) {
-        console.log(err);
-      } else {
-        console.log('Tweeted: ' + params.status);
-      }
-    })
-};
-*/
