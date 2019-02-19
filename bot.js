4adc9edc980a694e2ebd618f5cff7dca5409b860
asdf <rednuke123@gmail.com>
2019-02-18 23:40:19 -0600

    added command
    new command

--- bot.js
+++ bot.js
@@ -14,7 +14,9 @@ var T = new twit({
 
 
 var stream = T.stream('statuses/filter', { track: ['@FreeSpeech4You fosscad'] });
+var stream2 = T.stream('statuses/filter', { track: ['@FreeSpeech4You magazine'] });
 stream.on('tweet', tweetEvent);
+stream2.on('tweet', tweetEvent2);
 
 function tweetEvent(tweet) {
 
@@ -68,3 +70,54 @@ function tweetEvent(tweet) {
     })
 };
 
+function tweetEvent(tweet) {
+
+    // Who sent the tweet?
+    var name = tweet.user.screen_name;
+	var nameID  = tweet.id_str;
+	var quotedText = tweet.text;
+	var split = quotedText.split(" ");
+	var handles = [];
+	
+	//console.log(quotedText);
+	for(var i = 0; i < split.length-1; i++){
+		//console.log('split[i]: ' + split[i]);
+		if(split[i].startsWith("@")){
+			if(split[i] == '@freespeech4you'){
+			
+			}
+			else{
+				if(split[i] == '@FreeSpeech4You'){
+					
+				}else{
+				handles.push(split[i]);
+				}
+			}
+			//console.log('handles.tostring: ' + handles.toString());
+		}
+	}
+	
+    // What is the text?
+    // var txt = tweet.text;
+    // the status update or tweet ID in which we will reply
+    
+
+     // Get rid of the @ mention
+    // var txt = txt.replace(/@FreeSpeech4You/g, "");
+
+    // Start a reply back to the sender
+	var stringify = handles.toString();
+    var reply = "You want files to print a STANDARD CAPCAITY Magazine @" + name + ' ?' + '\nHere\s the files: https://github.com/maduce/fosscad-repo/tree/master/Rifles/AR-15_30_Round_Magazine-Ivanthetroll\n, along with proof of the mag in action: https://www.bitchute.com/video/VBzL8nKCCDzM/' + stringify;
+    var params             = {
+                              status: reply,
+                              in_reply_to_status_id: nameID
+                             };
+
+    T.post('statuses/update', params, function(err, data, response) {
+      if (err !== undefined) {
+        console.log(err);
+      } else {
+        console.log('Tweeted: ' + params.status);
+      }
+    })
+};
